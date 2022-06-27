8b3acdb86ed6945b835324079bbd203722ad577f
Teeceetec <onyiajustin@gmail.com>
2022-06-27 17:16:22 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300} />
+    <Image alt="" src={imageUrl} width={500} height={300} />
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -8,7 +8,7 @@ import { fetchApi,baseUrl } from '../../utils/fetchApi';
 
 const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
   <Box maxWidth='1000px' margin='auto' p='4'>
-    {photos && <ImageScrollbar data={photos} />}
+    {photos && <ImageScrollbar  data={photos} />}
     <Box w='full' p='6'>
       <Flex paddingTop='2' alignItems='center'>
         <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
